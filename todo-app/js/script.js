e5227ac9e83737af1219f1820e2c0099331c81e6
Tristan Wickliff <tristanlw20@gmail.com>
2017-06-18 20:03:47 -0700

    finished hw1

--- todo-app/js/script.js
+++ todo-app/js/script.js
@@ -1,28 +1,33 @@
 var tasklist = [];
 
 window.onload = function(){
-  console.log("in window.onload")
   drawToPage();
   var addtask = document.getElementById("addtask");
   addtask.onclick = updatePage;
 }
 function updatePage() {
-  console.log("pushing element to list");
   var task = document.getElementById("tasktxt").value;
   var ppl = document.getElementById("taskform").elements["owner"].value;
   var diff = document.getElementById("taskdiff").value;
-  console.log(tasklist);
   tasklist.push({task: task, owner:ppl, diff: diff});
   drawToPage();
   document.getElementById("tasktxt").value = "";
 }
 function drawToPage(){
-  console.log("in drawToPage");
   var todo = document.getElementById("todo");
   todo.innerHTML = "";
   for(var i = 0; i < tasklist.length; i++){
-    var newNode = document.createElement("p");
-    newNode.innerHTML = "Owner: " + tasklist[i].owner + " Difficulty: " + tasklist[i].diff +" Task: " + tasklist[i].task;
+    var newNode = document.createElement("li");
+    newNode.innerHTML = "Owner: " + tasklist[i].owner;
+    var smallNodes = document.createElement("ul");
+    newNode.appendChild(smallNodes);
+    var smallNode1 = document.createElement("li");
+    smallNodes.appendChild(smallNode1);
+    smallNode1.innerHTML = "Task: " + tasklist[i].task;
+    var smallNode2 = document.createElement("li");
+    smallNodes.appendChild(smallNode2);
+    smallNode2.innerHTML = "Difficulty: " + tasklist[i].diff;
     todo.appendChild(newNode);
     }
+  document.getElementById("taskform").reset();
   }
