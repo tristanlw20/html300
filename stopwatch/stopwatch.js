00c506f5127a8a65c2cb77843caeae6aa04c1d5d
Tristan Wickliff <tristanlw20@gmail.com>
2017-06-21 23:31:56 -0700

    chart added

--- stopwatch/stopwatch.js
+++ stopwatch/stopwatch.js
@@ -1,3 +1,17 @@
+const start = document.querySelector("button.start")
+const stop = document.querySelector("button.stop")
+
+
+const laplist = document.querySelector("#lapList")
+const stopwatchTime = document.querySelector("#stopwatchTime")
+
+const laps = []
+const intervalRate = 10
+
+let rawTime = 0
+let intervalID = null
+
+
 // turns the time into a human readable format
 function formatTime (raw) {
   let seconds = Math.floor(raw / 1000)
@@ -8,6 +22,22 @@ function formatTime (raw) {
   return `${zeroPad(minutes)}:${zeroPad(seconds.toFixed(2))}`
 }
 
+function stopwatchStart(event){
+	event.preventDefault()
+	console.log("started!")
+	intervalID = setInterval(stopwatchUpdate, intervalRate)
+}
+
+function stopwatchStop(event){
+	event.preventDefault()
+	console.log("stopped!")
+	clearInterval(intervalID)
+}
+function stopwatchUpdate(){
+	rawTime + intervalRate
+	stopwatchTime.innerHTML = formatTime(rawTime)
+}
+
 // adds a leading zero because humans like them
 function zeroPad (value) {
   let pad = value < 10 ? '0' : ''
@@ -16,4 +46,6 @@ function zeroPad (value) {
 
 document.addEventListener("DOMContentLoaded", function () {
   console.log('ready!')
+  start.addEventListener("click", stopwatchStart)
+  stop.addEventListener("click", stopwatchStop)
 })
--- todo-app/js/script.js
+++ todo-app/js/script.js
@@ -1,13 +1,20 @@
 var tasklist = [];
 
+// Load the Visualization API and the corechart package.
+google.charts.load('current', {'packages':['corechart']});
+
+document.getElementById("taskform").addEventListener("submit",function(event){
+  event.preventDefault()
+});
+
 function updatePage() {
-  event.preventDefault();
   var task = document.getElementById("tasktxt").value;
   var ppl = document.getElementById("taskform").elements["owner"].value;
   var diff = document.getElementById("taskdiff").value;
   tasklist.push({task: task, owner:ppl, diff: diff});
   drawToPage();
   document.getElementById("tasktxt").value = "";
+  document.getElementById("taskform").reset();
 }
 function drawToPage(){
   var todo = document.getElementById("todo");
@@ -24,6 +31,51 @@ function drawToPage(){
     smallNodes.appendChild(smallNode2);
     smallNode2.innerHTML = "Difficulty: " + tasklist[i].diff;
     todo.appendChild(newNode);
+
     }
-  document.getElementById("taskform").reset();
+     // Set a callback to run when the Google Visualization API is loaded.
+     google.charts.setOnLoadCallback(drawChart);
   }
+  
+      // Callback that creates and populates a data table,
+      // instantiates the pie chart, passes in the data and
+      // draws it.
+      function drawChart() {
+
+        var ec = 0;
+        var ac = 0;
+        var hc = 0;
+
+        if(tasklist.length>0){
+          for (var i = 0; i < tasklist.length; i++){
+            if (tasklist[i].diff == "Easy") {
+              ec+=1;
+            }else if (tasklist[i].diff == "Average"){
+              ac+=1;
+            }else if (tasklist[i].diff == "Hard"){
+              hc+=1;
+            }
+          }
+        }
+        
+        // Create the data table.
+        var data = new google.visualization.DataTable();
+        data.addColumn('string', 'Difficulty');
+        data.addColumn('number', 'Frequency');
+        
+        data.addRows([
+          ['Easy', ec],
+          ['Average', ac],
+          ['Hard', hc]
+        ]);
+        
+
+        // Set chart options
+        var options = {'title':'Ratio of Task Difficulty',
+                       'width':400,
+                       'height':300};
+
+        // Instantiate and draw our chart, passing in some options.
+        var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
+        chart.draw(data, options);
+      }
