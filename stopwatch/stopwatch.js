f02d2b83930fc53d674d1057275c21de72ce4d5c
Tristan Wickliff <tristanlw20@gmail.com>
2017-06-25 21:13:51 -0700

    stopwatch finished

--- stopwatch/stopwatch.js
+++ stopwatch/stopwatch.js
@@ -1,16 +1,17 @@
 const start = document.querySelector("button.start")
 const stop = document.querySelector("button.stop")
+const lap = document.querySelector("button.lap")
+const reset = document.querySelector("button.reset")
 
-
-const laplist = document.querySelector("#lapList")
+const lapList = document.querySelector("#lapList")
 const stopwatchTime = document.querySelector("#stopwatchTime")
 
-const laps = []
 const intervalRate = 10
 
+let laps = []
 let rawTime = 0
+let lapTime = 0
 let intervalID = null
-
 
 // turns the time into a human readable format
 function formatTime (raw) {
@@ -22,20 +23,19 @@ function formatTime (raw) {
   return `${zeroPad(minutes)}:${zeroPad(seconds.toFixed(2))}`
 }
 
-function stopwatchStart(event){
-	event.preventDefault()
-	console.log("started!")
+function stopwatchStart(){
+  stopwatchStop()
+  rawTime = 0
 	intervalID = setInterval(stopwatchUpdate, intervalRate)
 }
 
-function stopwatchStop(event){
-	event.preventDefault()
-	console.log("stopped!")
+function stopwatchStop(){
+	
 	clearInterval(intervalID)
 }
 function stopwatchUpdate(){
-	rawTime + intervalRate
-	stopwatchTime.innerHTML = formatTime(rawTime)
+	rawTime += intervalRate
+	stopwatchTime.innerHTML = formatTime(rawTime) 
 }
 
 // adds a leading zero because humans like them
@@ -44,8 +44,33 @@ function zeroPad (value) {
   return `${pad}${value}`
 }
 
+function lapping(){
+  clearInterval(intervalID)
+  let lapTime = formatTime(rawTime)
+  let lapName = prompt("First Name")
+  laps.push({name: lapName, intervalID: intervalID, time: lapTime})
+  lapList.innerHTML = ""
+  for (var i = 0; i < laps.length; i++){
+    var newNode = document.createElement("li")
+    newNode.innerHTML= `${laps[i].name} ${laps[i].time}`
+    lapList.appendChild(newNode)
+  }
+}
+
+function resets(){
+  rawTime = 0
+  stopwatchTime.innerHTML = formatTime(rawTime)
+  clearInterval(intervalID)
+  laps = []
+  lapList.innerHTML = ""
+}
+
 document.addEventListener("DOMContentLoaded", function () {
   console.log('ready!')
   start.addEventListener("click", stopwatchStart)
   stop.addEventListener("click", stopwatchStop)
+  lap.addEventListener("click", lapping)
+  reset.addEventListener("click", resets)
 })
+
+
